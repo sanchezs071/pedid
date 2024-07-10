400a6277549d8b69e3065ea0b359e28845e72f23
sanchezs071 <leonardopinho843@gmail.com>
2024-07-09 22:44:22 -0300

    Add files via upload

---
+++ pedido/script.js
@@ -0,0 +1,11 @@
+const sim = document.querySelector('#b1')
+const h1 = document.querySelector('#h1')
+
+
+function leo (){
+    h1.outerHTML = '<h1 id="h1">heheheheheheh vou cobra em</h1>'
+}
+
+
+
+sim.addEventListener('click',leo)
